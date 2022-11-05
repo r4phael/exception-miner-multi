057a65b8c78907be2fbf11e32f7ccac072d3af09
Jairo Souza <pto.raphael@gmail.com>
2022-11-04 23:32:21 -0300

    finishing the gh collector

--- collect_gh_projects.py
+++ collect_gh_projects.py
@@ -1,7 +1,10 @@
-import requests
+
+
 import logging
+import csv
+import time
+import requests
 import pandas as pd
-import tqdm
 
 logger = logging.getLogger()
 handler = logging.StreamHandler()
@@ -17,13 +20,14 @@ GITHUB_AUTH_HEADER = {
 }
 
 
-def search_github(min, max):
+def search_github(N):
+    '''Search github repos'''
     session = requests.Session()
 
     url = "https://api.github.com/search/repositories?"
     # querystring = {
     #    "q": f"language:python stars:{min}..{max}", "sort": "stars", "per_page": 100}
-    querystring = {"q": f"language:python",
+    querystring = {"q": f"language:python stars:<{N}",
                    "sort": "stars forks", "per_page": 100}
 
     first_page = session.get(url, params=querystring,)  # )
@@ -42,52 +46,58 @@ def search_github(min, max):
             break
 
 
-def get_next_page(page):
+def get_next_page(page) -> None:
+    '''Returns the next page of gh'''
     return page if page.headers.get('link') != None else None
 
 
-cols = ['owner', 'repo_name', 'url_repo', 'stars',
-        'size', 'watchers', 'language', 'has_issues', 'forks', 'open_issues']
+def main(n, iterations, headers):
+    '''
+    Iterate through pages, parse the results to a dataframe, and stop at 5000 repos.
+    '''
 
-df = pd.DataFrame(columns=cols)
+    with open("projects_gh.csv", 'w') as file:
+        writer = csv.writer(file, delimiter=',')
+        writer.writerow(headers)
 
-# Iterate through pages, parse the results to a dataframe, and stop at 5000 repos.
-MIN, MAX = 50, 500
+        for i in range(iterations):
+            logging.info("Iteration...... %s", i)
 
-for i in range(5):
-    for page in search_github(MIN, MAX):
-        try:
-            response_dict = page.json()
-            # Total repos: 43937
-            print("Total repos:", response_dict['total_count'])
-            # find total number of repositories
-            repos_dicts = response_dict['items']
-            print("Repos found:", len(repos_dicts))
-            # examine the first repository
+            if i != 0:
+                logging.info("Sleeping zZzZzZzZzZZZzZzZzZ...")
+                time.sleep(30)
 
-            repo_dict = repos_dicts
-            for repos_dict in repos_dicts:  # loop through all the dictionaries in repos_dicts.
-                # print('Owner:', repos_dict['owner']['login'])
-                # print('\nName:', repos_dict['name'])
-                # print('Repository:', repos_dict['html_url'])
-                # print('Stars:', repos_dict['stargazers_count'])
+            for page in search_github(n):
+                try:
+                    response_dict = page.json()
+                    #logging.info("Total repos:", response_dict['total_count'])
 
-                # print('Size:', repos_dict['size'])
-                # print('Watchers::', repos_dict['watchers_count'])
-                # print('Language:', repos_dict['language'])
-                # print('Has Issues:', repos_dict['has_issues'])
-                # print('Forks:', repos_dict['forks_count'])
-                # print('Open Issues:', repos_dict['open_issues_count'])
+                    # find total number of repositories
+                    repos_dicts = response_dict['items']
+                    #logging.info("Repos found:", len(repos_dicts))
 
-                df = pd.concat([df, pd.DataFrame(list([[repos_dict['owner']['login'], repos_dict['name'], repos_dict['html_url'], repos_dict['stargazers_count'],
-                                                        repos_dict['size'], repos_dict['watchers_count'], repos_dict[
-                                                            'language'], repos_dict['has_issues'],
-                                                        repos_dict['forks_count'], repos_dict['open_issues_count']]]), columns=cols)])
+                    # examine the first repository
+                    for repos_dict in repos_dicts:  # loop through all the dictionaries in repos_dicts.
 
-        except Exception as ex:
-            logging.info(
-                f"Error!!! parsing gh pages. exception: {str(ex)}")
-    MIN += 500
-    MAX += 500
+                        writer.writerow([repos_dict['owner']['login'], repos_dict['name'], repos_dict['html_url'], repos_dict['stargazers_count'],
+                                        repos_dict['size'], repos_dict['watchers_count'], repos_dict[
+                            'language'], repos_dict['has_issues'],
+                            repos_dict['forks_count'], repos_dict['open_issues_count'], 'github'])
 
-df.to_csv('projects_gh.csv')
+                except Exception as ex:
+                    logging.info(
+                        f"Error!!! parsing gh pages. exception: {str(ex)}")
+                    file.close()
+
+            df = pd.read_csv("projects_gh.csv")
+            n = df['stars'].min()
+
+    df.to_csv('projects_gh.csv')
+    df.loc[:, ['repo_name', 'url_repo', 'source']].to_csv(
+        'projects_py.csv', index=False)
+
+
+if __name__ == '__main__':
+    cols = ['owner', 'repo_name', 'url_repo', 'stars',
+            'size', 'watchers', 'language', 'has_issues', 'forks', 'open_issues', 'source']
+    main(n=9999999, iterations=5, headers=cols)
