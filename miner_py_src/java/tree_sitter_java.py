fca01a17c689169e9bde32cad3bb2f3f77623fa6
Tales Cunha <tvac@cin.ufpe.br>
2024-05-05 20:07:38 -0300

    Refactor Java code structure and file paths

--- miner_py_src/java/tree_sitter_java.py
+++ miner_py_src/java/tree_sitter_java.py
@@ -16,69 +16,100 @@ QUERY_METHOD_IDENTIFIER: Query = JAVA_LANGUAGE.query(
 QUERY_METHOD_BODY: Query = JAVA_LANGUAGE.query(
     """(method_declaration body: (block) @body)""")
 
+QUERY_METHOD_INVOCATION: Query = JAVA_LANGUAGE.query(
+    """(method_invocation (identifier) @method.invocation)""")
+
 QUERY_EXPRESSION_STATEMENT: Query = JAVA_LANGUAGE.query(
     """(expression_statement) @expression.stmt""")
 
 QUERY_TRY_STMT: Query = JAVA_LANGUAGE.query(
     """(try_statement) @try.statement""")
 
+
 QUERY_TRY_CATCH: Query = JAVA_LANGUAGE.query(
     """(try_statement
         (catch_clause)* @catch.clause) @try.stmt""")
+
 
 QUERY_CATCH_CLAUSE: Query = JAVA_LANGUAGE.query(
     """(catch_clause) @catch.clause""")
 
 QUERY_CATCH_IDENTIFIER_BODY: Query = JAVA_LANGUAGE.query(
     """
-    (catch_clause 
+     (catch_clause 
         (catch_formal_parameter 
-            (identifier) @catch.identifier)
+            (catch_type (type_identifier)? @catch.type)
+            (identifier)? @catch.identifier)
         (block) @catch.body
     )
     """
 )
 
-QUERY_CATCH_BLOCK: Query = JAVA_LANGUAGE.query(
-    """(catch_clause (block) @body)""")
+QUERY_METHOD_INVOCATION: Query = JAVA_LANGUAGE.query(
+    """(method_invocation (identifier) @method.invocation)""")
+
+
+QUERY_RETURN: Query = JAVA_LANGUAGE.query(
+    """(return_statement) @return.stmt""")
+
+QUERY_CATCH_BLOCK = JAVA_LANGUAGE.query("""
+    (catch_clause (catch_formal_parameter) @parameter (block) @body)
+""")
+
+QUERY_FIND_IDENTIFIERS = JAVA_LANGUAGE.query("""
+    (catch_type (type_identifier) @identifier)
+""")
 
 QUERY_CATCH_STATEMENTS: Query = JAVA_LANGUAGE.query(
     """(catch_clause (block [(_) @catch.expression]*))""")
+
+QUERY_INSTANCEOF_EXPRESSION: Query = JAVA_LANGUAGE.query(
+    """(instanceof_expression) @instanceof.expr""")
 
 QUERY_CATCH_EXPRESSION: Query = JAVA_LANGUAGE.query(
     """(catch_clause (_) @catch.expression)""")
 
 QUERY_FIND_IDENTIFIERS: Query = JAVA_LANGUAGE.query(
-    """(identifier) @identifier""")
+   """(type_identifier) @throw.identifier """)
 
 QUERY_THROW_STATEMENT: Query = JAVA_LANGUAGE.query(
     """(throw_statement) @throw.stmt""")
 
+# QUERY_THROW_STATEMENT_IDENTIFIER: Query = JAVA_LANGUAGE.query(
+#     """(throw_statement 
+# 	(object_creation_expression
+#                 (type_identifier) @throw.identifier
+#             ) @throw.identifier)""")
+
 QUERY_THROW_STATEMENT_IDENTIFIER: Query = JAVA_LANGUAGE.query(
     """(throw_statement 
-	(object_creation_expression
-                (type_identifier) @throw.identifier
-            ) @throw.identifier)""")
+    (object_creation_expression
+                (type_identifier) @throw.new.identifier
+            ) @throw.new.identifier)
+       (throw_statement 
+        (identifier) @throw.existing.identifier)""")
 
 QUERY_FINALLY_BLOCK: Query = JAVA_LANGUAGE.query(
     """ (finally_clause) @finally.clause""")
 
-QUERY_CATCH_ASSIGNMENT: Query = JAVA_LANGUAGE.query(
+QUERY_CATCH_ASSIGNMENT_EXPRESSION_LEFT: Query = JAVA_LANGUAGE.query(
     """(catch_clause 
         (catch_formal_parameter 
-            (identifier)? @catch.type 
-            (identifier)? @catch.identifier 
-        )
+            (identifier) @catch.identifier)
         (block
-            (local_variable_declaration
-                (type_identifier)? @type.identifier
-                (variable_declarator
-                    (identifier) @variable.identifier
-                    (method_invocation
-                        (identifier) @exception.identifier
-                        (identifier) @method.identifier
-                    )
+            (expression_statement
+                (assignment_expression 
+                    left: (identifier) @left.assignment.identifier
                 )
             )
-        )
+        ) @catch.body
     )""")
+
+
+QUERY_THROWABLE_GETCAUSE: Query = JAVA_LANGUAGE.query(
+    """(method_invocation 
+        (identifier) @method.identifier
+        (argument_list)
+    )"""
+)
+
